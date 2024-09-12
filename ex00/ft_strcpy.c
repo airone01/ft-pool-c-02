fad5e7e5bf409196726a8f5259eb0f922efe0bc1
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-12 20:18:40 +0200

    fix(ex00): simplify

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -10,32 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
+#include <stdio.h>
 
 char	*ft_strcpy(char *dest, char *src)
 {
+	int	i;
 	char	*rest;
-	char	cond;
 
 	rest = dest;
-	cond = '.';
-	while (cond != '\0')
+	i = 0;
+	while (src[i] != '\0')
 	{
-		*dest = *src;
-		cond = *dest++;
-		src++;
+		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
 	return (rest);
 }
 
-// int	main(void)
-// {
-// 	char	*src;
-// 	char	dest[10];
-// 	char	*rest;
-//
-// 	src = "hello";
-// 	printf("src: %s\n", src);
-// 	rest = ft_strcpy(dest, src);
-// 	printf("src: %s, dest: %s, rest: %s\n", src, dest, rest);
-// }
+int	main(void)
+{
+	char	*src;
+	char	dest[10];
+	char	*rest;
+
+	src = "hello";
+	printf("src: %s\n", src);
+	rest = ft_strcpy(dest, src);
+	printf("src: %s, dest: %s, rest: %s\n", src, dest, rest);
+}
