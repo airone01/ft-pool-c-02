a96e05df1733b92ba4e7f4d953d63cd1ea0cef5c
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-12 19:02:19 +0200

    fix(ex00): patch

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -10,21 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+// #include <stdio.h>
 
 char	*ft_strcpy(char *dest, char *src)
 {
+	char	*rest;
+
+	rest = dest;
 	while ((*dest++ = *src++) != '\0');
-	return (dest);
+	return (rest);
 }
 
-int	main(void)
-{
-	char	*src;
-	char	dest[10];
-
-	src = "hello";
-	printf("src: %s\n", src);
-	ft_strcpy(dest, src);
-	printf("src: %s, dest: %s\n", src, dest);
-}
+// int	main(void)
+// {
+// 	char	*src;
+// 	char	dest[10];
+// 	char	*rest;
+//
+// 	src = "hello";
+// 	printf("src: %s\n", src);
+// 	rest = ft_strcpy(dest, src);
+// 	printf("src: %s, dest: %s, rest: %s\n", src, dest, rest);
+// }
