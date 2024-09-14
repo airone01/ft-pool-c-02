937d4020e6b9d8ad5a28a6b1831bf056608163ff
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-14 19:50:29 +0200

    feat(ex11): implement

---
+++ ex11/ft_putstr_non_printable.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <erwann.lagouche@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 18:05:35 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/14 18:12:28 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+// #include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+// Print hexadecimal char* from integer
+void	puth(int nbr)
+{
+	if (nbr >= 16)
+	{
+		puth (nbr / 16);
+		ft_putchar ("0123456789abcdef"[nbr % 16]);
+		return ;
+	}
+	ft_putchar ("0123456789abcdef"[nbr % 16]);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	if (*str == '\0')
+		return ;
+	if (*str < ' ' || *str > '~')
+	{
+		ft_putchar ('\\');
+		if (((int) *str) < 16)
+			ft_putchar ('0');
+		puth ((int) *str);
+	}
+	else
+		ft_putchar (*str);
+	ft_putstr_non_printable(str + sizeof(char));
+}
+
+// int	main(void)
+// {
+// 	// printf ("PUTH: ");
+// 	// puth(1234);
+// 	ft_putstr_non_printable("Hello\n, world!");
+// }
